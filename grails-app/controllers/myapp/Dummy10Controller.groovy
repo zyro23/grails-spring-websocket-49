fe10ca79303cb71c92fd39eac6f961959c114553
zyro <zyro@zyro.net>
2017-05-17 10:35:10 +0200

    trying to repro zyro23/grails-spring-websocket#49

---
+++ grails-app/controllers/myapp/Dummy10Controller.groovy
@@ -0,0 +1,17 @@
+package myapp
+
+
+class Dummy10Controller {
+
+	def index1() {}
+	def index2() {}
+	def index3() {}
+	def index4() {}
+	def index5() {}
+	def index6() {}
+	def index7() {}
+	def index8() {}
+	def index9() {}
+	def index10() {}
+
+}
---
+++ grails-app/controllers/myapp/ExampleController.groovy
@@ -0,0 +1,17 @@
+package myapp
+
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+
+
+class ExampleController {
+
+	def index() {}
+
+	@MessageMapping("/hello")
+	@SendTo("/topic/hello")
+	protected String hello(String world) {
+		return "hello from controller, ${world}!"
+	}
+
+}
